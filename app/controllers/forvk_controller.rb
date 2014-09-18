81387066269b58dbd5bbe0bf3732511b628e075f
SkyDancer <SkyDancer718@gmail.com>
2014-09-18 23:10:12 +0900

    2

--- app/controllers/forvk_controller.rb
+++ app/controllers/forvk_controller.rb
@@ -7,11 +7,4 @@ class ForvkController < ApplicationController
   		#@users=	vk.users.get(uid: 155572362, fields: fields) 
 	end
 
-	def auth
-	session[:state] = Digest::MD5.hexdigest(rand.to_s)
-	redirect_to VkontakteApi.authorization_url(scope: 
-							[:notify, :friends, :photos], state: session[:state])
-	#@vk = VkontakteApi.authorize(code: params[:code])
-	end
-
 end
