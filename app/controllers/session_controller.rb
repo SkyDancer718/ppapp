23e30df60e66ad81cea43e000ec175be12877b4f
SkyDancer <SkyDancer718@gmail.com>
2014-09-22 15:18:39 +0900

    u

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -17,6 +17,9 @@ class SessionController < ApplicationController
 
     session[:token] = @vk.token
     session[:vk_id] = @vk.user_id
+
+    flash[:success] = @vk.token
+
     @staff = {token: session[:token], id: session[:id]}
     #User.new(id_vk: @vk.user_id.to_s, token: @vk.token.to_s)
 
