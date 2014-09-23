565eaa21724dd785521ab81d849e2e65c188a86d
SkyDancer <SkyDancer718@gmail.com>
2014-09-23 21:48:25 +0900

    vkgroup

--- app/controllers/forvk_controller.rb
+++ app/controllers/forvk_controller.rb
@@ -4,7 +4,7 @@ class ForvkController < ApplicationController
 	def index
 		#fields = [:first_name, :last_name, :photo_50]
   		#vk = VkontakteApi::Client.new
-  		#@users=	vk.users.get(uid: 155572362, fields: fields) 
+  		#@users=	vk.users.get(user_id: 155572362, fields: fields) 
 	end
 
 end
