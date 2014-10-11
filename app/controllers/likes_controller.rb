3534b19f95c45d6f9630945fa26dd21d44077401
SkyDancer <SkyDancer718@gmail.com>
2014-10-11 12:52:12 +0900

    remut

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -43,7 +43,7 @@ class LikesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_like
-      @like = Like.find(:hero_id session[:id], :victim_id like_params)
+      @like = Like.where(:hero_id session[:id], :victim_id like_params)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
